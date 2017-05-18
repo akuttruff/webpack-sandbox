0ba7c1d1139e59e6323a9e2cb0781d6bbafe406b
Amie Kuttruff <amie.kuttruff@gmail.com>
2017-05-17 20:04:08 -0700

    adds basic webpack config

---
+++ entry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './src/Home.js';
+
+require("./style.css");
+
+ReactDOM.render(<Hello />, document.body);
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hello(){
+  return (
+    <div> Hello </div>
+  );
+}
+
+export default Hello;
