fcd5a89b1d132bb559bd30aaba5cf7d8f404cdfd
Amie Kuttruff <amie.kuttruff@gmail.com>
2017-05-17 21:12:38 -0700

    adds babel presets; now able to parse JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-          { test: /\.css$/, 
-            loader: "style-loader!css-loader" 
-          },
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loaders: ["babel-loader"],
-          }
-        ]
-    }
+  entry: "./entry.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      }
+    ]
+  }
 };
